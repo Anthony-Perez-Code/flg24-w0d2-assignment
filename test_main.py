3ed16e453d81395062f509b8651661d77e7f909d
Anthony Perez <anthony.perez101417@gmail.com>
2024-09-18 09:19:50 -0400

    Updated message again

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ from main import hello_world
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(hello_world(), "Hello from Anthony!")
+        self.assertEqual(hello_world(), "Welcome to Git!")
 
 if __name__ == "__main__":
     unittest.main()
