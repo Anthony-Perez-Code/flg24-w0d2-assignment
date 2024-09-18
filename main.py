26d0498b8dd73881ba7b074a3542ebc3c632f91d
Anthony Perez <anthony.perez101417@gmail.com>
2024-09-18 09:18:10 -0400

    Added greeting

--- main.py
+++ main.py
@@ -2,5 +2,9 @@
 def hello_world():
     return "Hello from Anthony!"
 
+def greet_person(name):
+    return f"Hello, {name}!"
+
 if __name__ == "__main__":
     print(hello_world())
+    print(greet_person("Anthony"))
